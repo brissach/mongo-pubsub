ee2d0923cdcdc41a35f4540b332f4b213c5daf80
Clouke <76826179+Clouke@users.noreply.github.com>
2023-02-24 06:54:33 +0100

    create `KeyValue<K, V>`, add support for `Pairs<String, String>`

---
+++ src/main/java/gg/clouke/mps/KeyValue.java
@@ -0,0 +1,45 @@
+package gg.clouke.mps;
+
+import com.google.gson.Gson;
+import gg.acai.acava.collect.Mutability;
+import gg.acai.acava.collect.pairs.Pairs;
+
+/**
+ * @author Clouke
+ * @since 24.02.2023 06:45
+ * © mongo-pubsub - All Rights Reserved
+ */
+public class KeyValue<K, V> {
+
+  private final K key;
+  private final V value;
+
+  public KeyValue(K key, V value) {
+    this.key = key;
+    this.value = value;
+  }
+
+  public K getKey() {
+    return key;
+  }
+
+  public V getValue() {
+    return value;
+  }
+
+  public Pairs<String, String> pair() {
+    Gson gson = GsonSpec.getGson();
+    return Pairs.of(Mutability.IMMUTABLE,
+      gson.toJson(key, key.getClass()),
+      gson.toJson(value, value.getClass())
+    );
+  }
+
+  @Override
+  public String toString() {
+    return "KeyValue{" +
+        "key=" + key +
+        ", value=" + value +
+        '}';
+  }
+}
