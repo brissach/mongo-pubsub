91e17d9f5c75c4e74418beea4f9a7b75e1a4cc43
Clouke <76826179+Clouke@users.noreply.github.com>
2023-02-24 06:04:07 +0100

    refactor `MPSMongoClient` --> `MongoPubSubClient`

---
+++ src/main/java/gg/clouke/mps/MongoPubSubClient.java
@@ -0,0 +1,39 @@
+package gg.clouke.mps;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
+import java.util.Collections;
+
+/**
+ * @author Clouke
+ * @since 24.02.2023 05:12
+ * © mongo-pubsub - All Rights Reserved
+ */
+public class MongoPubSubClient {
+
+  private final MongoCollection<Document> publishers;
+
+  public MongoPubSubClient(String host, int port, String username, String password, String database) {
+    MongoClientSettings settings = MongoClientSettings.builder()
+            .applyToClusterSettings(builder -> builder.hosts(Collections.singletonList(new ServerAddress(host, port))))
+            .credential(MongoCredential.createCredential(username, database, password.toCharArray()))
+            .build();
+
+    try (MongoClient client = MongoClients.create(settings)) {
+      this.publishers = client
+              .getDatabase(database)
+              .getCollection("publishers");
+    }
+  }
+
+  public MongoCollection<Document> publishers() {
+    return publishers;
+  }
+
+}
