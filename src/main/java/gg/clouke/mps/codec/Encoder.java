fd9d08520455340d7b2a61b262baac8df2af89f2
Clouke <76826179+Clouke@users.noreply.github.com>
2023-02-25 15:18:10 +0100

    add simple encoders

--- src/main/java/gg/clouke/mps/codec/Encoder.java
+++ src/main/java/gg/clouke/mps/codec/Encoder.java
@@ -1,5 +1,7 @@
 package gg.clouke.mps.codec;
 
+import gg.clouke.mps.Payload;
+
 /**
  * @author Clouke
  * @since 24.02.2023 05:39
@@ -8,5 +10,29 @@ package gg.clouke.mps.codec;
 public abstract class Encoder<E, R> {
 
   public abstract R encode(E e);
+
+  public final Encoder<E, R> andThen(Encoder<R, R> after) {
+    return new Encoder<E, R>() {
+      @Override
+      public R encode(E e) {
+        return after.encode(Encoder.this.encode(e));
+      }
+    };
+  }
+
+  protected static final Encoder<String, String> IDENTITY = new Encoder<String, String>() {
+    @Override
+    public String encode(String s) {
+      return s;
+    }
+  };
+
+  protected static final Encoder<String, Payload> PAYLOAD = new Encoder<String, Payload>() {
+    @Override
+    public Payload encode(String s) {
+      return new Payload(s);
+    }
+  };
+
 
 }
