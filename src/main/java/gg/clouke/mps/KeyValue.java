adb1f9334f6abc74c7b1d7adbf073352808bf5aa
Clouke <76826179+Clouke@users.noreply.github.com>
2023-02-26 20:18:00 +0100

    deprecate & add docs

--- src/main/java/gg/clouke/mps/KeyValue.java
+++ src/main/java/gg/clouke/mps/KeyValue.java
@@ -5,10 +5,14 @@ import gg.acai.acava.collect.pairs.ImmutablePair;
 import gg.acai.acava.collect.pairs.Pairs;
 
 /**
+ * A key-value pair that can be used to send data in a message.
+ *
  * @author Clouke
  * @since 24.02.2023 06:45
+ * @deprecated Use {@link Pairs} instead.
  * © mongo-pubsub - All Rights Reserved
  */
+@Deprecated
 public class KeyValue<K, V> {
 
   private final K key;
@@ -19,14 +23,29 @@ public class KeyValue<K, V> {
     this.value = value;
   }
 
+  /**
+   * Gets the key of the key-value pair.
+   *
+   * @return the key of the key-value pair.
+   */
   public K getKey() {
     return key;
   }
 
+  /**
+   * Gets the value of the key-value pair.
+   *
+   * @return the value of the key-value pair.
+   */
   public V getValue() {
     return value;
   }
 
+  /**
+   * Converts the key-value pair to a pair of strings.
+   *
+   * @return a pair of strings.
+   */
   public Pairs<String, String> pair() {
     Gson gson = GsonSpec.getGson();
     return new ImmutablePair<>(
@@ -35,6 +54,11 @@ public class KeyValue<K, V> {
     );
   }
 
+  /**
+   * Converts the key-value pair to a string.
+   *
+   * @return the string representation of the key-value pair.
+   */
   @Override
   public String toString() {
     return "KeyValue{" +
