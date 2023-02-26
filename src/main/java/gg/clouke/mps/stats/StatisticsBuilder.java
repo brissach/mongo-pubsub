15376efa6a6af465f37e1571d084d4cc4e674a0d
Clouke <76826179+Clouke@users.noreply.github.com>
2023-02-26 15:15:54 +0100

    create `StatisticsBuilder`

---
+++ src/main/java/gg/clouke/mps/stats/StatisticsBuilder.java
@@ -0,0 +1,12 @@
+package gg.clouke.mps.stats;
+
+/**
+ * @author Clouke
+ * @since 26.02.2023 15:15
+ * © mongo-pubsub - All Rights Reserved
+ */
+public class StatisticsBuilder {
+
+  private int sizeOfNetworkUpdates = 40;
+
+}
