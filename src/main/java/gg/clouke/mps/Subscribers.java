51c12a6080a565d3686855e15b358025c483cb03
Clouke <76826179+Clouke@users.noreply.github.com>
2023-02-26 20:03:46 +0100

    add docs

--- src/main/java/gg/clouke/mps/Subscribers.java
+++ src/main/java/gg/clouke/mps/Subscribers.java
@@ -5,6 +5,34 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * A house-holding class for all subscribers.
+ *
+ * <p>Responsible for holding all subscribers and
+ * internally dispatching messages to them.
+ *
+ * <p>Holds an instance of the {@link Subscribers} class, which can be accessed
+ * through the {@link #getInstance()} method.
+ *
+ * <p><strong>NOTE:</strong> cannot be instantiated multiple times.
+ *
+ * <p><b>Example usage:</b></p>
+ * <h4>- Functional listener:
+ * <pre>{@code
+ * Subscribers subscribers = new Subscribers();
+ * subscribers()
+ *   .listenDirectly("my-listener",
+ *     payload -> {
+ *       System.out.println(payload.toString());
+ *       payload.close();
+ *     });
+ * }
+ * </pre>
+ * <h4>- Class listener:
+ * <pre>{@code
+ * Subscribers subscribers = new Subscribers();
+ * subscribers.addListener(new MyTestSub());
+ * }</pre>
+ *
  * @author Clouke
  * @since 25.02.2023 10:43
  * © mongo-pubsub - All Rights Reserved
