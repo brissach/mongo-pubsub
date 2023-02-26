1f822ac454f44c0b9a0f51380119db05a92d0c0f
Clouke <76826179+Clouke@users.noreply.github.com>
2023-02-26 20:15:02 +0100

    add docs

--- src/main/java/gg/clouke/mps/GsonSpec.java
+++ src/main/java/gg/clouke/mps/GsonSpec.java
@@ -8,6 +8,8 @@ import gg.acai.acava.caches.CacheLoader;
 import java.util.Map;
 
 /**
+ * A utility class that provides a Gson instance and TypeTokens.
+ *
  * @author Clouke
  * @since 24.02.2023 06:16
  * © mongo-pubsub - All Rights Reserved
@@ -21,18 +23,38 @@ public final class GsonSpec {
     .setPrettyPrinting()
     .create();
 
+  /**
+   * Gets the pretty printing Gson instance.
+   *
+   * @return the pretty printing Gson instance.
+   */
   public static Gson getPrettyPrintingGson() {
     return PRETTY_PRINTING_GSON;
   }
 
+  /**
+   * Gets the Gson instance.
+   *
+   * @return the Gson instance.
+   */
   public static Gson getGson() {
     return GSON;
   }
 
+  /**
+   * Gets the TypeToken for the CacheLoader class.
+   *
+   * @return the TypeToken for the CacheLoader class.
+   */
   public static TypeToken<CacheLoader> getCacheLoaderToken() {
     return CACHE_LOADER_TOKEN;
   }
 
+  /**
+   * Gets the TypeToken for the Map<String, String> class.
+   *
+   * @return the TypeToken for the Map<String, String> class.
+   */
   public static TypeToken<Map<String, String>> getPayloadToken() {
     return PAYLOAD_TOKEN;
   }
