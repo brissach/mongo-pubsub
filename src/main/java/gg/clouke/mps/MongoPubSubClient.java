acf710ae9286f396ffc643e1032f6caaf04d4b04
Clouke <76826179+Clouke@users.noreply.github.com>
2023-02-26 17:52:53 +0100

    add docs

--- src/main/java/gg/clouke/mps/MongoPubSubClient.java
+++ src/main/java/gg/clouke/mps/MongoPubSubClient.java
@@ -20,6 +20,22 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 /**
+ * Represents the main class of the mongo-pubsub library.
+ *
+ * <p>Responsible for managing the connection to the database and the collection,
+ * allowing interaction with the mongo client, watcher and the subscribers.
+ *
+ * <p>Holds an instance of the {@link MongoPubSubClient} class, which can be accessed
+ * through the {@link #getInstance()} method.
+ *
+ * <p><strong>NOTE:</strong> cannot be instantiated directly, use {@link
+ * #newBuilder()} to create a new instance.
+ *
+ * <p><b>Throws:</b>
+ * <ul>
+ *   <li>{@link IllegalStateException} if the client is already initialized.</li>
+ * </ul>
+ *
  * @author Clouke
  * @since 24.02.2023 05:12
  * © mongo-pubsub - All Rights Reserved
@@ -29,10 +45,20 @@ public final class MongoPubSubClient implements Closeable {
 
   private static MongoPubSubClient INSTANCE;
 
+  /**
+   * Gets the instance of the {@link MongoPubSubClient} class.
+   *
+   * @return the instance of this client.
+   */
   public static MongoPubSubClient getInstance() {
     return INSTANCE;
   }
 
+  /**
+   * Creates a new instance of the {@link MongoPubSubClient} class.
+   *
+   * @return a new instance of this client.
+   */
   public static MongoClientBuilder newBuilder() {
     return new MongoClientBuilder();
   }
@@ -42,6 +68,13 @@ public final class MongoPubSubClient implements Closeable {
   private final MongoCollection<Document> publishers;
   private final Subscribers subscribers;
 
+  /**
+   * Constructs a new instance of the {@link MongoPubSubClient} class.
+   * delegates the construction from the {@link MongoClientBuilder}.
+   *
+   * @param b the builder to construct the client.
+   * @throws IllegalStateException if the client is already initialized.
+   */
   public MongoPubSubClient(MongoClientBuilder b) {
     if (INSTANCE != null)
       throw new IllegalStateException("MongoPubSubClient is already initialized.");
@@ -54,6 +87,9 @@ public final class MongoPubSubClient implements Closeable {
       .getDatabase(b.database)
       .getCollection("publishers");
 
+    /*
+     * Check for the clearPreviousIndexes flag, if true, drop the index
+     */
     if (b.clearPreviousIndexes) {
       publishers.dropIndex(Indexes.ascending("payload:send"));
     }
@@ -71,26 +107,53 @@ public final class MongoPubSubClient implements Closeable {
     this.subscribers = new Subscribers();
   }
 
+  /**
+   * Gets the mongo client.
+   *
+   * @return the mongo client.
+   */
   @Nonnull
   public MongoClient client() {
     return client;
   }
 
+  /**
+   * Gets the publishers collection.
+   *
+   * @return the publishers collection.
+   */
   @Nonnull
   public MongoCollection<Document> publishers() {
     return publishers;
   }
 
+  /**
+   * Gets the watcher.
+   *
+   * @return the watcher.
+   */
   @Nonnull
   public CollectionWatcher watcher() {
     return watcher;
   }
 
+  /**
+   * Gets the subscribers.
+   *
+   * @return the subscribers.
+   */
   @Nonnull
   public Subscribers subscribers() {
     return subscribers;
   }
 
+  /**
+   * Updates the flush after write time.
+   *
+   * @param time the time to flush after.
+   * @param unit the unit of the time.
+   * @return the instance of this client.
+   */
   public MongoPubSubClient updateFlushAfterWrite(long time, TimeUnit unit) {
     publishers.dropIndex(Indexes.ascending("payload:send"));
     publishers.createIndex(Indexes
@@ -99,6 +162,11 @@ public final class MongoPubSubClient implements Closeable {
     return this;
   }
 
+  /**
+   * Flushes the publishers collection.
+   *
+   * @return the size of the collection before flushing.
+   */
   public long flush() {
     long size = -1L;
     boolean count = false;
@@ -116,6 +184,13 @@ public final class MongoPubSubClient implements Closeable {
     return size;
   }
 
+  /**
+   * Enqueues a new payload to the publishers collection.
+   *
+   * @param target the target of the payload.
+   * @param payload the payload to enqueue.
+   * @return Returns a waiter that can be used to ensure the payload being sent with thread locking before closing the client
+   */
   public Waiter enqueue(@Nonnull String target, Payload payload) {
     Requisites.requireNonNull(target, "target cannot be null.");
     Document document = payload.asDocument()
@@ -126,6 +201,12 @@ public final class MongoPubSubClient implements Closeable {
     return watcher().waiter();
   }
 
+  /**
+   * Internal procedure to build the mongo client settings.
+   *
+   * @param b the builder to construct the client.
+   * @return the mongo client settings.
+   */
   private MongoClientSettings doBuildProcedure(MongoClientBuilder b) {
     MongoClientSettings.Builder builder = MongoClientSettings.builder();
     if (b.uri != null) {
@@ -139,6 +220,9 @@ public final class MongoPubSubClient implements Closeable {
     return builder.build();
   }
 
+  /**
+   * Closes the client.
+   */
   @Override
   public void close() {
     synchronized (this) {
