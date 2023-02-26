b415b9eb229ec85f73af9e42ca6091cd5ddd881c
Clouke <76826179+Clouke@users.noreply.github.com>
2023-02-26 14:43:38 +0100

    create `Statistics`

---
+++ src/main/java/gg/clouke/mps/stats/Statistics.java
@@ -0,0 +1,14 @@
+package gg.clouke.mps.stats;
+
+import gg.acai.acava.commons.graph.Graph;
+
+/**
+ * @author Clouke
+ * @since 26.02.2023 14:42
+ * © mongo-pubsub - All Rights Reserved
+ */
+public interface Statistics {
+
+  Graph<Number> network();
+
+}
